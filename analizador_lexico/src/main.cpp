3fff890d94af0f4d8a5af21df0ed776ea64514e3
Daniel <danielnoronha57@gmail.com>
2026-09-12 19:44:48 -0300

    configuração dos tipos de tokens

--- analizador_lexico/src/main.cpp
+++ analizador_lexico/src/main.cpp
@@ -1,12 +1,22 @@
 #include <FlexLexer.h>
+#include <iostream>
 #include "token.h"
+using std::cout;
+
+extern int yycolumn;
 
 int main(int argc, char** argv)
 {
 	yyFlexLexer lexer;
-	unsigned int lookahead;
+	int lookahead;
 
-	while ((lookahead = lexer.yylex()) != 0) {
-		
+	while ((lookahead = lexer.yylex()) > 0) {
+		token t = { (uint)lookahead, lexer.YYText(), lexer.lineno(), yycolumn };
+		cout << t.type << " " << t.lexeme << " " << t.line << " " << t.column << std::endl;
+	}
+
+	if (lookahead == -1) {
+		cout << "Erro na linha: " << lexer.lineno() << ".\n" << "Coluna: " << yycolumn << ".\n" 
+			<< "Caractere não reconhecido: \"" << lexer.YYText() << "\"\n";
 	}
 }
