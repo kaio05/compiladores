3b6fadd5b48768092ebda96da3fb7d87af9ca78b
Kaio Silva <kaiofernando05cont@gmail.com>
2026-09-14 11:30:37 -0300

    adiciona leitura de arquivos

--- analizador_lexico/src/main.cpp
+++ analizador_lexico/src/main.cpp
@@ -1,5 +1,8 @@
 #include <FlexLexer.h>
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
 #include "token.h"
 using std::cout;
 
@@ -7,8 +10,22 @@ extern int yycolumn;
 
 int main(int argc, char** argv)
 {
+	if(argc != 2) {
+		std::cerr << "Missing argument\n";
+		return 1;
+	}
+
+	std::ifstream file(argv[1]);
+
+	if(!file.is_open()) {
+		std::cerr << "Error opening file\n";
+		return 1;
+	}
+
 	yyFlexLexer lexer;
 	int lookahead;
+
+	lexer.switch_streams(&file, &std::cout);
 
 	while ((lookahead = lexer.yylex()) > 0) {
 		token t = { (uint)lookahead, lexer.YYText(), lexer.lineno(), yycolumn };
