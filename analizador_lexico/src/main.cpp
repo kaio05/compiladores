3f4219c98abf80472456752941b4907a63ed3122
Daniel <danielnoronha57@gmail.com>
2026-09-16 16:03:28 -0300

    classificação de tokens

--- analizador_lexico/src/main.cpp
+++ analizador_lexico/src/main.cpp
@@ -6,43 +6,59 @@
 
 extern int yycolumn;
 
+uint classifyTokens(uint type)
+{
+    if (type >= tokenClass::RESERVED_WORDS && type < tokenClass::PRIMITIVE_TYPE) {
+        return tokenClass::RESERVED_WORDS;
+   } 
+    else if (type >= tokenClass::PRIMITIVE_TYPE && type < tokenClass::META_ATTRIBUTE) {
+        return tokenClass::PRIMITIVE_TYPE;
+   } 
+    else if (type >= tokenClass::META_ATTRIBUTE && type < tokenClass::CLASS_STEREOTYPES) {
+        return tokenClass::CLASS_STEREOTYPES;
+   }
+    else if (type >= tokenClass::CLASS_STEREOTYPES && type < tokenClass::RELATION_STEREOTYPES) {
+        return tokenClass::CLASS_STEREOTYPES;
+   }
+    else if (type >= tokenClass::RELATION_STEREOTYPES && type < tokenClass::NUMBERL) {
+        return tokenClass::RELATION_STEREOTYPES;
+   }
+
+    return type;
+};
+
 int main(int argc, char** argv)
 {
 	if(argc < 2) {
 		std::cerr << "Argumento faltando\n" << "Uso: ./lexer <arquivo.tonto>\n";
 		return 1;
 	} else if (argc > 2) {
-		std::cerr << "Muitos argumentos: " << argc-1 << "\n";
+		std::cerr << "Muitos argumentos: " << argc-1 << ". Espera-se apenas 1 argumento: <arquivo.tonto>\n";
 		return 1;
 	}
 
 	std::ifstream file(argv[1]);
-
 	if(!file.is_open()) {
-		std::cerr << "Error opening file\n";
+		std::cerr << "Erro ao abrir o arquivo!\n";
 		return 1;
 	}
 
 	std::unordered_map<int, int> tokenCountMap;
-
 	yyFlexLexer lexer;
 	int lookahead;
 
 	lexer.switch_streams(&file, &std::cout);
 
 	std::cout << "===============\n"
-		 << "Visão analítica\n"
-		 << "===============\n";
+		      << "Visão analítica\n"
+		      << "===============\n";
 
 	while ((lookahead = lexer.yylex()) > 0) {
-		token t = { (uint)lookahead, lexer.YYText(), lexer.lineno(), yycolumn };
-		tokenCountMap[t.type]++;
-		std::cout << "<Token: " << tokenTypeNames[t.type] << ", \"" << t.lexeme << "\", lin: " << t.line << ", col:" << t.column << ">\n";
-	}
+		tokenCountMap[classifyTokens(lookahead)]++;
 
-	if (lookahead == -1) {
-		std::cerr << "Erro na linha: " << lexer.lineno() << ".\n" << "Coluna: " << yycolumn << ".\n" 
-			<< "Caractere não reconhecido: \"" << lexer.YYText() << "\"\n";
+		std::cout << "<Token: " 
+			<< tokenTypeNames[lookahead] << ", \"" 
+			<< lexer.YYText() << "\", lin: " << lexer.lineno() << ", col:" << yycolumn << ">\n";
 	}
 
 	if (lookahead == 0) {
@@ -53,10 +69,7 @@ int main(int argc, char** argv)
 		<< "=================\n";
 
 		for(auto pair: tokenCountMap) {
-			std::cout << tokenTypeNames[pair.first]
-				<< ": "
-				<< pair.second
-				<< std::endl;
+			std::cout << tokenClassNames.at(pair.first) << ": "<< pair.second << std::endl;
 		}
 	}
 }
--- analizador_lexico/src/token.h
+++ analizador_lexico/src/token.h
@@ -1,33 +1,39 @@
 #pragma once
 
 #include <string>
+#include <map>
+using std::map;
 using std::string;
 using uint = unsigned int;
 
-// Tipos que os token podem assumir.
 enum tokenTypes {
+    //Palavras reservadas
     GENSET = 1, DISJOINT, COMPLETE, GENERAL, SPECIFICS, WHERE, PACKAGE, IMPORT, FUNCTIONAL_COMPLEXES, 
-
-    ORDERED, CONST, DERIVED, SUBSETS, REDEFINES,
-
+    
+    // Tipos 
     NUMBER, STRING, BOOLEAN, DATE, TIME, DATETIME,
 
+    // Meta atributo
+    ORDERED, CONST, DERIVED, SUBSETS, REDEFINES,
+
+    // Estereotipos de classe
     EVENT, SITUATION, PROCESS, CATEGORY, MIXIN, PHASE_MIXIN, ROLE_MIXIN, HISTORICAL_MIXIN, 
     HISTORICAL_ROLE, HISTORICAL_ROLE_MIXIN, KIND, COLLECTIVE, QUANTITY, QUALITY, MODE, 
     INTRINSIC_MODES, EXTRINSIC_MODES, SUBKIND, PHASE, ROLE, 
     
+    // Estereotipos de relação
     MATERIAL, DERIVATION, COMPARATIVE, MEDIATION, CHARACTERIZATION, EXTERNAL_DEPENDENCE, 
     COMPONENT_OF, MEMBER_OF, SUB_COLLECTION_OF, SUB_QUALITY_OF, INSTANTIATION, TERMINATION, PARTICIPATIONAL, 
     PARTICIPATION, HISTORICAL_DEPENDENCE, CREATION, MANIFESTATION, BRINGS_ABOUT, TRIGGERS, COMPOSITION, 
     AGGREGATION, INHERENCE, VALUE, FORMAL, CONSTITUTION, 
 
-    SPECIAL_SYMBOL, NEW_TYPE, NUMBER_LITERAL, INSTANCE_NAME, CLASS_NAME, RELATION_NAME,
+    NUMBER_LITERAL, SPECIAL_SYMBOL, NEW_TYPE, INSTANCE_NAME, CLASS_NAME, RELATION_NAME,
 
     UNKNOWN
 };
 
 const string tokenTypeNames[] = {
-    "EOF", 
+    "EOF"
 
     "GENSET", "DISJOINT", "COMPLETE", "GENERAL", "SPECIFICS", "WHERE", "PACKAGE", "IMPORT", "FUNCTIONAL_COMPLEXES", 
 
@@ -49,25 +55,41 @@ const string tokenTypeNames[] = {
     "UNKNOWN"
 };
 
-enum tokenTypesClassification {
+enum tokenClass {
+    // Inicio das palavras reservadas.
     RESERVED_WORDS = tokenTypes::GENSET,
-    TYPE_NAME = tokenTypes::NUMBER,
+
+    // Fim das palavras reservadas, inicio dos tipos.
+    PRIMITIVE_TYPE = tokenTypes::NUMBER,
+
+    // Fim dos tipos, inicio de meta atributos.
     META_ATTRIBUTE = tokenTypes::ORDERED,
+
+    // Fim dos meta atributos, inicio dos estereotipos de classe.
     CLASS_STEREOTYPES = tokenTypes::EVENT,
+
+    // Fim dos estereotipos de classe, inicio dos estereotipos de relação.
     RELATION_STEREOTYPES = tokenTypes::MATERIAL,
-    NUMBERLITERAL = tokenTypes::NUMBER_LITERAL,
-    SPECIALSYMBOL = tokenTypes::SPECIAL_SYMBOL,
-    NEWTYPE = tokenTypes::NEW_TYPE,
-    INSTANCE = tokenTypes::INSTANCE_NAME,
-    CLASSES = tokenTypes::CLASS_NAME,
-    RALATION = tokenTypes::RELATION_NAME
+
+    // Fim dos estereotipos de relação, inicio dos tipos de token unicos.
+    NUMBERL = tokenTypes::NUMBER_LITERAL,
+
+    SPECIALSYMBOL, NEWTYPE, INSTANCE, CLASS, RELATION, 
+
+    UNKNOWNSYMBOL
 };
 
-
-// Token
-struct token {
-    uint type;
-    string lexeme;
-    int line;
-    int column;
+const map<uint, string> tokenClassNames = {
+    { RESERVED_WORDS, "Palavra reservada" },
+    { PRIMITIVE_TYPE, "Tipo primitivo" },
+    { META_ATTRIBUTE, "Meta atributo" },
+    { CLASS_STEREOTYPES, "Estereótipos de classe" },
+    { RELATION_STEREOTYPES, "Estereótipos de relação" },
+    { NUMBERL, "Número" },
+    { SPECIALSYMBOL, "Símbolo especial" },
+    { NEWTYPE, "Novo tipo" },
+    { INSTANCE, "Instância" },
+    { CLASS, "Classe" },
+    { RELATION, "Relação" },
+    { UNKNOWNSYMBOL, "Símbolo desconhecido" }
 };
